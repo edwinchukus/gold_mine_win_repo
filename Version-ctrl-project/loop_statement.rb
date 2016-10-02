e5baca3bd8b82a60284a12efcbdeaeb0e654c7d2
Ifeanyi Nwosu <ifeanyinwosu76@gmail.com>
2016-10-02 13:09:15 +0100

    Some loop statment added

---
+++ Version-ctrl-project/loop_statement.rb
@@ -0,0 +1,41 @@
+
+begin
+  i = 0
+  for i in 0...10
+    puts i + 1
+
+  end
+end
+
+
+begin
+  house  = 1
+  while house >= 10
+    puts("house")
+
+    house = house +1
+  end
+end
+
+
+
+begin
+
+  lotto = 100.to_i
+  for lotto in 100...120
+    puts 100 + 1
+
+  end
+  = end
+
+i = 0
+for i in 0...10
+  puts i + 1
+
+  begin
+
+    spirit_man = 10
+    for spirit_man in 10 ...100
+      puts spirit_man = spirit_man + 1
+
+    end
