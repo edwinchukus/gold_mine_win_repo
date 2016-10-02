687afba09a39902d8aa080982f21ed0093788001
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-10-02 18:05:23 +0100

    Corection for Nduka

--- class_work/week.rb
+++ class_work/week.rb
@@ -1,13 +1,17 @@
 puts "enter activity name"
 
-activity_name = gets().chomp
+activity_name = gets().chomp.to_s
 
-if(activity_name.eql?football)
+
+sport = "football"
+home = "tv"
+work = "working"
+
+if(activity_name.eql?sport)
   puts"I am playing football"
-
-elsif (activity_name.eql?working)
+elsif (activity_name.eql?work)
   puts"i am working today"
-elsif(activity_name.eql?tv)
+elsif(activity_name.eql?home)
   puts("i am watching tv")
 else
   puts"just looking"
