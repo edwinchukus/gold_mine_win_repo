cf0501308ef0f35424bc592bbce827f0339c7d2e
Sylvester Nduka <fnduka1@gmail.com>
2016-10-09 00:23:00 +0100

    Added files

--- class_work/week.rb
+++ class_work/week.rb
@@ -2,6 +2,7 @@ puts "enter activity name"
 
 activity_name = gets().chomp.to_s
 
+<<<<<<< HEAD
 sport = "football"
 home = "tv"
 work = "working"
@@ -11,6 +12,18 @@ if(activity_name.include?sport)
 elsif (activity_name.include?work)
   puts"i am working today"
 elsif(activity_name.include?home)
+=======
+
+sport = "football"
+home = "tv"
+work = "working"
+
+if(activity_name.eql?sport)
+  puts"I am playing football"
+elsif (activity_name.eql?work)
+  puts"i am working today"
+elsif(activity_name.eql?home)
+>>>>>>> bee879633be9ac4e239efa339eea5c9d8e1b7ede
   puts("i am watching tv")
 else
   puts"just looking"
