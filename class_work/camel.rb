601849592cfc3d0ca9c19c40782445902d1c8484
Sylvester Nduka <fnduka1@gmail.com>
2016-10-09 00:35:05 +0100

    modified file

--- class_work/camel.rb
+++ class_work/camel.rb
@@ -1,33 +1,15 @@
-=begin
+#Guys look at this code "I tried to use comparison and assignment together but not working"
 puts"enter the name of a student"
 student_name = gets().chomp.to_s
 
-if(student_name.eql?"James")
+if(student_name.eql?.casecmp"James")
   puts"He is not from Africa"
-elsif(student_name.eql?"Ekenne")
+elsif(student_nameeql?.casecmp"Ekenne")
   puts"This student is from Africa"
-elsif(student_name.eql?"Chong")
+elsif(student_nameeql?.casecmp"Chong")
   puts"A chinese name"
 else
   puts"you need to look at the list properly"
-=end
-
-# Trying to use comparison and assignment together
-puts"enter student name"
-
-student_name = gets().chomp
-
-case student_name
-  when "James","james"
-    puts"english"
-    exit
-  when "Eke","eke"
-    puts"igbo"
-    exit
-  when "Chong","chong"
-    puts"chinese"
-    exit
-  else
-    puts"not among the name"
-
 end
+
+
