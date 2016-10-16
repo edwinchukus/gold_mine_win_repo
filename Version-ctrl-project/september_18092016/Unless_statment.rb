d274c9803eeb75125b99f1a12f75402fe70c7a78
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-10-16 17:43:41 +0100

    corrections

--- Version-ctrl-project/september_18092016/Unless_statment.rb
+++ Version-ctrl-project/september_18092016/Unless_statment.rb
@@ -1,16 +1,16 @@
 
 puts "Which skin colour do not crack?"
 skin_colour = gets.chomp.to_s
-skin_colour = "black"
+
+
+
+
 unless
 skin_colour.eql? "black"
   puts "This skin colour is beautiful and does not crack"
 else
   skin_colour.eql? "white"
   puts "This skin colour type cracks easily"
-  elsif skin_colour.eql? "mixed race"
-  puts " beautiful skin collour"
-  else puts " your colour is confused"
 end
 
 
