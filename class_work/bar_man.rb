316118a0c0884b6648684aad60cff186fecd47d3
Augustine Nwafor <ucheaustine2002@yahoo.com>
2016-10-28 14:56:38 +0100

    lessons

---
+++ class_work/bar_man.rb
@@ -0,0 +1,14 @@
+class BarMan
+
+
+  puts "please enter first name"
+  first_name = gets.chomp.to_i
+
+  puts "please enter second name"
+  second_name = gets.chomp.to_i
+
+  result = first_name + second_name
+  puts result
+
+
+end
---
+++ class_work/if_else_statement.rb
@@ -0,0 +1,13 @@
+
+weather = "sunny"
+
+
+if(weather.eql? "raining")
+
+  puts("The road will be sliping because it is raining")
+
+else(weather.eql? "sunny")
+
+  puts("The road will be good to drive")
+
+end
