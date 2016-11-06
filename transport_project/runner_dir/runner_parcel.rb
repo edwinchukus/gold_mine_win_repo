2daba8774a63b5a4d7469f1148d7040b1247558e
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-11-06 18:57:23 +0000

    Update codes

--- transport_project/runner_dir/runner_parcel.rb
+++ transport_project/runner_dir/runner_parcel.rb
@@ -1,6 +1,6 @@
 require_relative '../getters_and_setters_concept/royal_mail_parcel'
 
-puts("Enter the Length and Width")
+puts("Enter the Length")
 length = gets.chomp.to_i
 puts("Enter the Width")
 width = gets.chomp.to_i
@@ -20,12 +20,17 @@ new_length = gets.chomp.to_i
 my_letter.set_length(new_length)
 puts("Enter new width?")
 
+
+
+
+
+# This is the new Calling of the length and width codes
 new_width = gets.chomp.to_i
 my_letter.set_width(new_width)
+
 
 my_letter_length_1 = my_letter.get_length
 my_letter_width_1 = my_letter.get_width
 
 area1 = my_letter_length_1 * my_letter_width_1
-
 puts(area1)
