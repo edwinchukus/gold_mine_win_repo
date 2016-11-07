5fd52927ead61911b5bc38512f04e64eea0be261
Ifeanyi Nwosu <ifeanyinwosu76@gmail.com>
2016-11-07 12:31:25 +0000

    added idea

---
+++ Maths_Project/runnerfile_maths.rb
@@ -0,0 +1,13 @@
+require_relative '../Maths_Project/arithmatic'
+
+  calculation = Arithmatic.new()
+  #calculation.multiply_three_numbers_1
+
+  calculation = Arithmatic.new()
+  calculation.multiple_three_numbers_2(2, 3, 4)
+
+ # calculation.multiply_three_numbers_3(1, 2, 3)
+
+
+
+
