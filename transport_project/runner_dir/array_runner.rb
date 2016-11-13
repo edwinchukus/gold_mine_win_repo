9e057d4242160bba35a629a8a3a54a5435a10aa5
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-11-13 18:57:54 +0000

    Revised getters and setter, attr writer & reader, attr accessor & arrays in Ruby

---
+++ transport_project/runner_dir/array_runner.rb
@@ -0,0 +1,25 @@
+require_relative '../arrays/school'
+
+
+
+
+# my_schl_det = school_details[3]
+#  puts(my_schl_det)
+
+
+# school_details.each do |my_schl_det|
+#     puts(my_schl_det)
+# end
+
+
+# my_sons = School.new
+#
+# my_sons.search_school("Bedford")
+
+
+pupil_ages = [5, 5, 6, 8, 4, 6, 5, 7]
+
+puts(pupil_ages.count)
+
+
+
