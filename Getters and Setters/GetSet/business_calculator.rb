32b0bfaf5d2e011074057546e186a2fb8a10dbea
Nduka Nwapanyi <fnduka1@gmail.com>
2016-11-13 16:00:31 +0000

    modified

--- Getters and Setters/GetSet/business_calculator.rb
+++ Getters and Setters/GetSet/business_calculator.rb
@@ -2,9 +2,11 @@ class BusinessCalculator
 
 
 
-  def initialize(num_1, num_2, operator)
+  def initialize(num_1, num_2,num_3,num_4, operator)
     @first_num = num_1
     @second_num = num_2
+    @third_num = num_3
+    @four_num = num_4
     @operator = operator
   end
 
@@ -12,9 +14,9 @@ class BusinessCalculator
 
   def process_two_numbers()
     if(@operator.eql?("+"))
-      my_result = @first_num.to_i + @second_num.to_i
+      my_result = @first_num.to_i + @second_num.to_i - @third_num.to_i
     elsif(@operator.eql?("-"))
-      my_result = @first_num.to_i - @second_num.to_i
+      my_result = @first_num.to_i - @second_num.to_i / @four_num.to_i
     elsif(@operator.eql?("/"))
       my_result = @first_num.to_i / @second_num.to_i
     elsif(@operator.eql?("*"))
@@ -31,8 +33,7 @@ end
 
 
 
-my_num_combination = BusinessCalculator.new(7, 6, "/")
+my_num_combination = BusinessCalculator.new(7, 6,4,3, "/")
 
 result = my_num_combination.process_two_numbers
-
 puts(result.to_f)
